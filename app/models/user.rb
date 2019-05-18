e6c9adab8437cd1354ba3733efab0f37a650a8db
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-05-18 07:03:04 +0000

    [modified]  app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
 
   has_many :addresses, dependent: :destroy
-  accepts_nested_attributes_for :address
+  accepts_nested_attributes_for :addresses
   has_many :carts, dependent: :destroy
   has_many :payments, dependent: :destroy
 
