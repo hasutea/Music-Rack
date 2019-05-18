8f56d6834621d5c233768417ef12e656bbe99ce1
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-05-18 07:57:42 +0000

    [modified]  app/controllers/admins/users_controller.rb

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -6,7 +6,7 @@ class Admins::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @address = @user.addresses
+    @addresses = @user.addresses
   end
 
   def edit
