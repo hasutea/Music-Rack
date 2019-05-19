cd47a3edfcd754af34095e09e06c8615bbda4786
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-05-19 05:05:45 +0000

    [modified] app/controllers/users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   def show
+  	@user = User.find(params[:id])
+  	@address = @user.addresses.where('create_at > ?', 1.day.ago).first
   end
 
   def edit
