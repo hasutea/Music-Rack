39fcf0b30c309f686e9c4c22a5e78aabd60b88dd
Yuko <naka.u926@gmail.com>
2019-05-19 05:47:40 +0000

    [Update]/products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
   def index
-  	@products = Product.page(params[:page]).reverse_order
+    @products = Product.page(params[:page]).reverse_order
+
+    # 検索値を検索するインスタンス変数
+    @query = Product.ransack(params[:q])
+    # 検索結果を表示するインスタンス変数
+    @products = @query.paginate(page: params[:page], per_page: 20)
   end
 
   def show
