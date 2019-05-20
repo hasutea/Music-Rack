69012ccc12a080e21bd1dd3f226c4b81d55150aa
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-05-20 09:31:01 +0000

    [modified] app/models/admin.rb

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,6 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, length: {in: 8..32}
 end
