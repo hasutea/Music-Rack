0a9558e7ed75be56dde7cff98aa9a05d498ca1b4
ishitsukayuki <19961017yuki@gmail.com>
2019-05-22 19:19:27 +0900

    [Modify]payment new.html.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :carts, dependent: :destroy
   has_many :payments, dependent: :destroy
-
+  accepts_nested_attributes_for :addresses, allow_destroy: true
 end
