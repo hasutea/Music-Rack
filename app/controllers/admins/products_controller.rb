42b7f7e2d6b02454983ee45896987ad1a9fa8938
Yuko <naka.u926@gmail.com>
2019-05-23 15:04:43 +0000

    [Update]admins/products

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -41,10 +41,6 @@ class Admins::ProductsController < ApplicationController
     end
   end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
-
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,12 @@
 class CartsController < ApplicationController
+
   before_action :authenticate_user!, :validate_cart!
 
   def show
     # カートに入れるボタンを押された商品全てを表示(カートテーブルの商品id,ユーザーid,数量を取得して表示)
     @cart = current_user.cart
-    @carts = Cart.all
+    @cart_products = @cart.products
+    @total_price = @cart_products.joins(:product).sum(:price)
 
     # カート内に商品がなかったら、商品がないと表示
   end
@@ -12,8 +14,8 @@ class CartsController < ApplicationController
   def create
     # カートの中身を登録 商品詳細ページでカートに入れるボタンを押したら「カートに商品を追加しました」と表示させてそのページに留まる
     cart = current_user.cart
-    cart = Cart.new(cart_params)
-    if cart.save
+    cart_product = Cart.new(cart_params)
+    if cart_product.save
       redirect_to product_path(@product.id)
     else
       render :show
@@ -42,6 +44,11 @@ class CartsController < ApplicationController
 
   def cart_params
     params.require(:cart).permit(:product_id, :user_id, :quantity)
+  end
+
+  def validate_cart!
+    @cart = current_user.cart
+    @params_cart = Cart.find(params[:id])
   end
 
 end
