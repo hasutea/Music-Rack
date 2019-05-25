2c5dae218205789f84237cd2b28540cd0bb10e94
ishitsukayuki <19961017yuki@gmail.com>
2019-05-25 14:47:58 +0900

    [Modify]payments new.html.erb

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -21,6 +21,6 @@ class PaymentsController < ApplicationController
 
 private
   def payment_params
-    params.require(:payment).permit(:shipping_last_name, :shipping_first_name, :shipping_postal_code, :shipping_address, :order_status, :payment_method_id, purchase_products_attributes: [:id, :product_id, :quantity, :price])
+    params.require(:payment).permit(:shipping_last_name, :shipping_first_name, :shipping_postal_code, :shipping_address, :order_status, :payment_method_id, :user_id, purchase_products_attributes: [:id, :product_id, :quantity, :price])
   end
 end
