ae302ebdb9e9fa2198058821239b094237f01919
komarinko <49143273+komarinko@users.noreply.github.com>
2019-05-28 15:33:06 +0900

    Update application_controller_renderer.rb

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,4 +6,3 @@
 #     https: false
 #   )
 # end
-Refile.secret_key = '3a110cd5d1fe4abea374633555fb07544b63775c84d79c849b751a2d9f0d9e7acc53535db65e8be23336c230d4bc9fdb3e7d3736733a1c950a50d8c3b3011e64'
