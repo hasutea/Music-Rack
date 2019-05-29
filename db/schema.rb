623fbf09ec5d48cace399e1581d01486526189e3
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-05-29 05:23:55 +0000

    [modified] db/migrate/20190505042301_devise_create_users.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_040451) do
+ActiveRecord::Schema.define(version: 2019_05_28_072359) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2019_05_29_040451) do
   create_table "carts", force: :cascade do |t|
     t.integer "product_id"
     t.integer "user_id"
-    t.integer "quantity"
+    t.integer "quantity", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -124,13 +124,13 @@ ActiveRecord::Schema.define(version: 2019_05_29_040451) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "last_name"
     t.string "first_name"
     t.string "kana_last_name"
     t.string "kana_first_name"
     t.string "telephone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
